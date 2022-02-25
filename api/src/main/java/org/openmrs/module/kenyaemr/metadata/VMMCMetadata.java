f5a58fd079d251d5ec4cd22e293e76739b2dc094
Patrick <gpatryllus@gmail.com>
2022-02-25 12:13:35 +0300

    Added other treatments in vmmcMedicalHistory Examination form

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/VMMCMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/VMMCMetadata.java
@@ -27,11 +27,13 @@ public class VMMCMetadata extends AbstractMetadataBundle {
 	public static final class _EncounterType {
 		public static final String VMMC_DISCONTINUATION = "4f02dfed-a2ec-40c2-b546-85dab5831871";
 		public static final String VMMC_ENROLLMENT = "85019fbe-9339-49f7-8341-e9a04311bb99";
+		public static final String VMMC_MEDICAL_HISTORY_EXAMINATION = "a2010bf5-2db0-4bf4-819f-8a3cffbcb21b";
 	}
 
 	public static final class _Form {
 		public static final String VMMC_DISCONTINUATION_FORM = "bc6a9e7d-58f7-43c0-8334-d8011fef4000";
 		public static final String VMMC_ENROLLMENT_FORM = "a74e3e4a-9e2a-41fb-8e64-4ba8a71ff984";
+		public static final String VMMC_MEDICAL_HISTORY_EXAMINATION_FORM = "d42aeb3d-d5d2-4338-a154-f75ddac78b59";
 	}
 
 
@@ -50,9 +52,11 @@ public class VMMCMetadata extends AbstractMetadataBundle {
 	public void install() {
 		install(encounterType("VMMC Enrollment", "Enrollment onto VMMC program", _EncounterType.VMMC_ENROLLMENT));
 		install(encounterType("VMMC Discontinuation", "Discontinuation from VMMC program", _EncounterType.VMMC_DISCONTINUATION));
+		install(encounterType("VMMC Medical History and Examination", "VMMC Medical History and Examination", _EncounterType.VMMC_MEDICAL_HISTORY_EXAMINATION));
 
 		install(form("VMMC Enrollment Form", null, _EncounterType.VMMC_ENROLLMENT, "1", _Form.VMMC_ENROLLMENT_FORM));
 		install(form("VMMC Discontinuation Form", null, _EncounterType.VMMC_DISCONTINUATION, "1", _Form.VMMC_DISCONTINUATION_FORM));
+		install(form("VMMC Medical History and Examination Form", null, _EncounterType.VMMC_MEDICAL_HISTORY_EXAMINATION, "1", _Form.VMMC_MEDICAL_HISTORY_EXAMINATION_FORM));
 
 
 		//Installing identifiers
