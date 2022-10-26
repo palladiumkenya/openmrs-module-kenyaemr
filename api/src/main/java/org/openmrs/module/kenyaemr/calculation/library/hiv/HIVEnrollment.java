bd6272eb111d42e1aca90ad45485de0d2db67b39
gabriel090 <gabriel.mbatha@gmail.com>
2022-10-26 07:46:37 +0300

    Calculations to Checks if a patient is enrolled in HIV program

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/HIVEnrollment.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/HIVEnrollment.java
@@ -1,15 +1,4 @@
-/**
- * This Source Code Form is subject to the terms of the Mozilla Public License,
- * v. 2.0. If a copy of the MPL was not distributed with this file, You can
- * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
- * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
- *
- * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
- * graphic logo is a trademark of OpenMRS Inc.
- */
-package org.openmrs.module.kenyaemr.calculation.library.hiv;
-
-import org.apache.commons.logging.Log;
+package org.openmrs.module.kenyaemr.calculation.library.hiv;import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Program;
 import org.openmrs.calculation.patient.PatientCalculationContext;
@@ -19,18 +8,15 @@ import org.openmrs.module.kenyacore.calculation.BooleanResult;
 import org.openmrs.module.kenyacore.calculation.Filters;
 import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
 /**
- * Checks if a patient is enrolled in HIV program
+ * Calculations to Checks if a patient is enrolled in HIV program
  */
 public class HIVEnrollment extends AbstractPatientCalculation {
-
     protected static final Log log = LogFactory.getLog(HIVEnrollment.class);
-
     @Override
     public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
         Program hivProgram = MetadataUtils.existing(Program.class, HivMetadata._Program.HIV);
