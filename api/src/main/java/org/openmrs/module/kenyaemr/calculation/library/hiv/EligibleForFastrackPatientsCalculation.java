73cbbe6b2a65b00fb5d05f8d3510fe8d111ba8a6
makombe <kennedymakombe@gmail.com>
2024-08-28 17:50:59 +0300

    Fix fast track form eligibility calculation to use appoitment from bahmni

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/EligibleForFastrackPatientsCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/EligibleForFastrackPatientsCalculation.java
@@ -13,7 +13,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
-import org.openmrs.Concept;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.Program;
@@ -21,6 +20,9 @@ import org.openmrs.api.ConceptService;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.appointments.service.AppointmentServiceDefinitionService;
+import org.openmrs.module.appointments.service.AppointmentsService;
+import org.openmrs.module.appointments.model.Appointment;
 import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
 import org.openmrs.module.kenyacore.calculation.BooleanResult;
 import org.openmrs.module.kenyacore.calculation.CalculationUtils;
@@ -33,9 +35,12 @@ import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.openmrs.module.kenyaemr.util.EmrUtils;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 
+
 import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -44,7 +49,7 @@ import java.util.Set;
  * longer follow-up intervals (depends on clinician ==>longer than 30 days)
  *
  **/
-public class EligibleForFastrackPatientsCalculation extends AbstractPatientCalculation  {
+public class EligibleForFastrackPatientsCalculation extends AbstractPatientCalculation {
 
     protected static final Log log = LogFactory.getLog(EligibleForFastrackPatientsCalculation.class);
     static ConceptService conceptService = Context.getConceptService();
@@ -52,47 +57,58 @@ public class EligibleForFastrackPatientsCalculation extends AbstractPatientCalcu
     @Override
     public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
 
-        Concept TCAdate = Dictionary.getConcept(Dictionary.RETURN_VISIT_DATE);
+        String hivConsultation = "885b4ad3-fd4c-4a16-8ed3-08813e6b01fa";
+        AppointmentsService appointmentsService = Context.getService(AppointmentsService.class);
+        AppointmentServiceDefinitionService apptServiceDefinitionService = Context.getService(AppointmentServiceDefinitionService.class);
         Program hivProgram = MetadataUtils.existing(Program.class, HivMetadata._Program.HIV);
         Set<Integer> alive = Filters.alive(cohort, context);
         Set<Integer> inHivProgram = Filters.inProgram(hivProgram, alive, context);
         Set<Integer> ltfu = CalculationUtils.patientsThatPass(calculate(new LostToFollowUpCalculation(), cohort, context));
         CalculationResultMap ret = new CalculationResultMap();
 
+
         for(Integer ptId: cohort){
-
             Integer tcaPlus30days = 0;
-            Integer tcaConcept = 5096;
-            Date tcaDate = null;
             Date tcaObsDate = null;
+            Date appointmentDate = null;
             boolean patientInHivProgram = false;
-
             boolean eligible = false;
-            Date currentDate =new Date();
+            List<Appointment> appointments = appointmentsService.getAllFutureAppointmentsForService(apptServiceDefinitionService.getAppointmentServiceByUuid(hivConsultation));
+            
+            // Check if patient has a future appointment and get the appointment date
+            Optional<Appointment> futureAppointment = appointments.stream()
+                .filter(appointment -> appointment.getPatient().getPatientId().equals(ptId))
+                .findFirst();
 
-         //With Greencard TCA more than 30 days
+            if (futureAppointment.isPresent()) {
+                appointmentDate = futureAppointment.get().getStartDateTime();
+                
+            }
+
+            //With Greencard TCA more than 30 days
             Encounter lastFollowUpEncounter = EmrUtils.lastEncounter(Context.getPatientService().getPatient(ptId), Context.getEncounterService().getEncounterTypeByUuid("a0034eee-1940-4e35-847f-97537a35d05e"));   //last greencard followup form
             if (lastFollowUpEncounter != null) {
                 for (Obs obs : lastFollowUpEncounter.getObs()) {
-                    if (obs.getConcept().getConceptId().equals(tcaConcept)){
-                         tcaDate = obs.getValueDatetime();
-                         tcaObsDate = obs.getObsDatetime();
-                         tcaPlus30days= daysBetween(tcaDate,tcaObsDate);
-                       }
+                    tcaObsDate = obs.getObsDatetime();
+                    if(appointmentDate != null && tcaObsDate != null) {
+                        tcaPlus30days = daysBetween(appointmentDate, tcaObsDate);
+                    }
                 }
             }
 
             if (inHivProgram.contains(ptId) && !ltfu.contains(ptId)) {
                 patientInHivProgram = true;
             }
-            if(patientInHivProgram && tcaDate != null && tcaObsDate != null && tcaDate.after(new Date()) && tcaPlus30days >= 30) { // exclude missed appointments
+
+            if(patientInHivProgram && appointmentDate != null && tcaObsDate != null && appointmentDate.after(new Date()) && tcaPlus30days >= 30) {
                 eligible = true;
             }
             ret.put(ptId, new BooleanResult(eligible, this));
         }
         return ret;
     }
-    private Integer daysBetween(Date d1, Date d2){
+
+    private Integer daysBetween(Date d1, Date d2) {
         DateTime date1 = new DateTime(d1.getTime());
         DateTime date2 = new DateTime(d2.getTime());
         return Math.abs(Days.daysBetween(date1, date2).getDays());
