769cc0118f997054e5a64d40fb35d94222fbcfb0
patryllus <gpatryllus@gmail.com>
2025-03-03 10:45:17 +0300

    Added endpoint to provide case surveillance indicators, denominoators and threshhold

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/FacilityDashboardUtil.java
@@ -0,0 +1,302 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.util.PrivilegeConstants;
+
+public class FacilityDashboardUtil {
+
+	/**
+	 * Surveillance dashboards
+	 * 1. Count of clients tested positive not linked
+	 * @return long value
+	 */
+	public static Long getHivPositiveNotLinked() {		
+		String hivPositiveNotLinkedQuery = "SELECT COUNT(a.patient_id) number_hiv_positive\n" +
+			"FROM ((SELECT av.patient_id\n" +
+			"       FROM kenyaemr_etl.etl_mch_antenatal_visit av\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics a on av.patient_id = a.patient_id\n" +
+			"       WHERE av.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND av.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT d.patient_id\n" +
+			"       FROM kenyaemr_etl.etl_mchs_delivery d\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics a on a.patient_id = d.patient_id\n" +
+			"       WHERE d.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND d.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT p.patient_id\n" +
+			"       FROM kenyaemr_etl.etl_mch_postnatal_visit p\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics d on p.patient_id = d.patient_id\n" +
+			"       WHERE p.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND p.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT t.patient_id\n" +
+			"       FROM kenyaemr_etl.etl_hts_test t\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics d on d.patient_id = t.patient_id\n" +
+			"           AND t.final_test_result = 'Positive'\n" +
+			"           AND t.voided = 0\n" +
+			"           AND t.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE)) a\n" +
+			"         LEFT JOIN\n" +
+			"     (SELECT l.patient_id, l.ccc_number\n" +
+			"      FROM kenyaemr_etl.etl_hts_referral_and_linkage l\n" +
+			"      WHERE date(l.visit_date) BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"      GROUP BY l.patient_id) l ON a.patient_id = l.patient_id\n" +
+			"         LEFT JOIN (SELECT e.patient_id\n" +
+			"                    FROM kenyaemr_etl.etl_hiv_enrollment e\n" +
+			"                    WHERE date(e.visit_date) BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE) e\n" +
+			"                   ON e.patient_id = a.patient_id\n" +
+			"where l.patient_id is null\n" +
+			"  and e.patient_id is null;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long hivPositiveNotLinkedTotal = (Long) Context.getAdministrationService().executeSQL(hivPositiveNotLinkedQuery, true).get(0).get(0);			
+			return hivPositiveNotLinkedTotal;
+			
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 1.1 Count of clients tested positive baseline
+	 * @return long value
+	 */
+	public static Long getPatientsTestedHivPositive() {
+		String hivTestedPositiveQuery = "SELECT COUNT(patient_id) number_hiv_positive\n" +
+			"FROM ((SELECT av.patient_id, av.encounter_id\n" +
+			"       FROM kenyaemr_etl.etl_mch_antenatal_visit av\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics a on av.patient_id = a.patient_id\n" +
+			"       WHERE av.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND av.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT d.patient_id, d.encounter_id\n" +
+			"       FROM kenyaemr_etl.etl_mchs_delivery d\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics a on a.patient_id = d.patient_id\n" +
+			"       WHERE d.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND d.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT p.patient_id, p.encounter_id\n" +
+			"       FROM kenyaemr_etl.etl_mch_postnatal_visit p\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics d on p.patient_id = d.patient_id\n" +
+			"       WHERE p.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"         AND p.final_test_result = 'Positive')\n" +
+			"      UNION\n" +
+			"      (SELECT t.patient_id, t.encounter_id\n" +
+			"       FROM kenyaemr_etl.etl_hts_test t\n" +
+			"                inner join kenyaemr_etl.etl_patient_demographics d on d.patient_id = t.patient_id \n" +
+			"         AND t.final_test_result = 'Positive'\n" +
+			"         AND t.voided = 0\n" +
+			"         AND t.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE)) a;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long hivTestedPositiveTotal = (Long) Context.getAdministrationService().executeSQL(hivTestedPositiveQuery, true).get(0).get(0);
+			return hivTestedPositiveTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 2. Pregnant post partum women at High risk not linked to prep
+	 * @return long value
+	 */
+	public static Long getPregnantPostpartumNotInPrep() {	
+		String pregnantPostPartumNotPrepLinkedQuery = "SELECT COUNT(a.patient_id) high_risk_not_on_PrEP\n" +
+			"FROM (SELECT s.patient_id\n" +
+			"      FROM kenyaemr_etl.etl_hts_eligibility_screening s\n" +
+			"               INNER JOIN (SELECT t.patient_id,\n" +
+			"                                  max(t.visit_date)                                           AS hts_date,\n" +
+			"                                  mid(max(concat(date(visit_date), t.final_test_result)), 11) AS hiv_test_results,\n" +
+			"                                  mid(max(concat(date(visit_date), t.hts_entry_point)), 11)   AS entry_point,\n" +
+			"                                  t.visit_date\n" +
+			"                           from kenyaemr_etl.etl_hts_test t\n" +
+			"                           GROUP BY t.patient_id\n" +
+			"                           HAVING hts_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE\n" +
+			"                              AND hiv_test_results = 'Negative'\n" +
+			"                              AND entry_point in (160538, 160456, 1623)) t\n" +
+			"                          ON s.patient_id = t.patient_id -- AND s.visit_date <= t.visit_date\n" +
+			"    where s.hts_risk_category IN ('High', 'Very high') and s.currently_on_prep in ('NO','Declined to answer')\n" +
+			"         AND s.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY) AND CURRENT_DATE) a\n" +
+			"         left join (select e.patient_id,\n" +
+			"                           max(e.visit_date)                                        as latest_enrollment_date,\n" +
+			"                           f.latest_fup_date,\n" +
+			"                           greatest(ifnull(f.latest_fup_app_date, '0000-00-00'),\n" +
+			"                                    ifnull(latest_refill_app_date, '0000-00-00'))   as latest_appointment_date,\n" +
+			"                           greatest(ifnull(latest_fup_date, '0000-00-00'),\n" +
+			"                                    ifnull(latest_refill_visit_date, '0000-00-00')) as latest_visit_date,\n" +
+			"                           r.latest_refill_visit_date,\n" +
+			"                           f.latest_fup_app_date,\n" +
+			"                           r.latest_refill_app_date,\n" +
+			"                           d.latest_disc_date,\n" +
+			"                           d.disc_patient\n" +
+			"                    from kenyaemr_etl.etl_prep_enrolment e\n" +
+			"                             left join\n" +
+			"                         (select f.patient_id,\n" +
+			"                                 max(f.visit_date)                                      as latest_fup_date,\n" +
+			"                                 mid(max(concat(f.visit_date, f.appointment_date)), 11) as latest_fup_app_date\n" +
+			"                          from kenyaemr_etl.etl_prep_followup f\n" +
+			"                          where f.visit_date <= date(CURRENT_DATE)\n" +
+			"                          group by f.patient_id) f on e.patient_id = f.patient_id\n" +
+			"                             left join (select r.patient_id,\n" +
+			"                                               max(r.visit_date)                                      as latest_refill_visit_date,\n" +
+			"                                               mid(max(concat(r.visit_date, r.next_appointment)), 11) as latest_refill_app_date\n" +
+			"                                        from kenyaemr_etl.etl_prep_monthly_refill r\n" +
+			"                                        where r.visit_date <= date(CURRENT_DATE)\n" +
+			"                                        group by r.patient_id) r on e.patient_id = r.patient_id\n" +
+			"                             left join (select patient_id                                               as disc_patient,\n" +
+			"                                               max(d.visit_date)                                        as latest_disc_date,\n" +
+			"                                               mid(max(concat(d.visit_date, d.discontinue_reason)), 11) as latest_disc_reason\n" +
+			"                                        from kenyaemr_etl.etl_prep_discontinuation d\n" +
+			"                                        where d.visit_date <= date(CURRENT_DATE)\n" +
+			"                                        group by patient_id\n" +
+			"                                        having latest_disc_date <= date(CURRENT_DATE)) d\n" +
+			"                                       on e.patient_id = d.disc_patient\n" +
+			"                    group by e.patient_id\n" +
+			"                    having timestampdiff(DAY, date(latest_appointment_date), date(CURRENT_DATE)) <= 7\n" +
+			"                       and date(latest_appointment_date) >= date(latest_visit_date)\n" +
+			"                       and ((latest_enrollment_date >= d.latest_disc_date\n" +
+			"                        and latest_appointment_date > d.latest_disc_date) or d.disc_patient is null)) b\n" +
+			"                   on a.patient_id = b.patient_id\n" +
+			"where b.patient_id is null;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long pregnantPostPartumNotPrepLinkedTotal = (Long) Context.getAdministrationService().executeSQL(pregnantPostPartumNotPrepLinkedQuery, true).get(0).get(0);			
+			return pregnantPostPartumNotPrepLinkedTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 3. Delayed viral load testing. Eligible without VL test taken
+	 * @return long value
+	 */
+	public static Long getEligibleForVlSampleNotTaken() {		
+		String eligibleForVlSampleNotTakenQuery = "select count(t.patient_id) from kenyaemr_etl.etl_hts_test t left join\n" +
+			"         (select l.patient_id, l.ccc_number from kenyaemr_etl.etl_hts_referral_and_linkage l \n" +
+			"                  where date(l.visit_date) <= date('2020-01-01') group by l.patient_id) l on t.patient_id = l.patient_id\n" +
+			"                    left join (select e.patient_id from kenyaemr_etl.etl_hiv_enrollment e \n" +
+			"                               where date(e.visit_date) <= date('2020-01-01'))e on e.patient_id = t.patient_id\n" +
+			"                                 where t.final_test_result='Positive' and t.test_type = 2 \n" +
+			"                                 and date(t.visit_date) between date('2020-01-01') and date('2025-01-01') and l.ccc_number is null and e.patient_id is null;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long eligibleForVlSampleNotTakenTotal = (Long) Context.getAdministrationService().executeSQL(eligibleForVlSampleNotTakenQuery, true).get(0).get(0);			
+			return eligibleForVlSampleNotTakenTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 4. Count of Virally suppressed clients with +2 weeks without EAC
+	 * @return long value
+	 */
+	public static Long getVirallySuppressedWithoutEAC(){		
+		String getVirallySuppressedWithoutEACQuery = "select count(b.patient_id) as unsuppressed_no_eac\n" +
+			"from (select x.patient_id as patient_id,DATE_SUB(CURRENT_DATE, INTERVAL 21 DAY), DATE_SUB(CURRENT_DATE, INTERVAL 14 DAY)\n" +
+			"      from kenyaemr_etl.etl_laboratory_extract x\n" +
+			"      where x.lab_test = 856\n" +
+			"        and test_result >= 200\n" +
+			"        and x.date_test_result_received BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 21 DAY) AND DATE_SUB(CURRENT_DATE, INTERVAL 14 DAY)) b\n" +
+			"         left join (select e.patient_id, e.visit_date as eac_date\n" +
+			"                    from kenyaemr_etl.etl_enhanced_adherence e\n" +
+			"                    where e.visit_date BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL 14 DAY) AND CURRENT_DATE) e\n" +
+			"                   on b.patient_id = e.patient_id\n" +
+			"where e.patient_id is null;\n";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long virallySuppressedWithoutEACQueryTotal = (Long) Context.getAdministrationService().executeSQL(getVirallySuppressedWithoutEACQuery, true).get(0).get(0);			
+			return virallySuppressedWithoutEACQueryTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 5. Count of HEI 6-8 weeks without DNA PCR results
+	 * @return long value
+	 */
+	public static Long getHeiSixToEightWeeksWithoutPCRResults() {		
+		String heiSixToEightWeeksWithoutPCRResultsQuery = "SELECT count(e.patient_id) as hei_without_pcr\n" +
+			"FROM kenyaemr_etl.etl_hei_enrollment e\n" +
+			"         INNER JOIN kenyaemr_etl.etl_patient_demographics d on e.patient_id = d.patient_id\n" +
+			"         LEFT JOIN kenyaemr_etl.etl_hiv_enrollment hiv on e.patient_id = hiv.patient_id\n" +
+			"         LEFT JOIN(SELECT x.patient_id week6pcr, x.test_result as week6results\n" +
+			"                   FROM kenyaemr_etl.etl_laboratory_extract x\n" +
+			"                   WHERE x.lab_test = 1030\n" +
+			"                     AND x.order_reason = 1040) t ON e.patient_id = t.week6pcr\n" +
+			"WHERE TIMESTAMPDIFF(WEEK, d.DOB, DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)) BETWEEN 6 AND 8\n" +
+			"  AND hiv.patient_id IS NULL\n" +
+			"  AND t.week6pcr IS NULL;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long heiSixToEightWeeksWithoutPCRResultsTotal = (Long) Context.getAdministrationService().executeSQL(heiSixToEightWeeksWithoutPCRResultsQuery, true).get(0).get(0);
+			return heiSixToEightWeeksWithoutPCRResultsTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	/**
+	 * Surveillance dashboards
+	 * 6. Count of HEI 24 months without Documented Outcome
+	 * @return long value
+	 */
+	public static Long getHei24MonthsWithoutDocumentedOutcome() {
+		String hei24MonthsWithoutDocumentedOutcomeQuery = "SELECT count(e.patient_id) as hei_without_outcome\n" +
+			"FROM kenyaemr_etl.etl_hei_enrollment e\n" +
+			"         INNER JOIN kenyaemr_etl.etl_patient_demographics d ON d.patient_id = e.patient_id\n" +
+			"         LEFT JOIN (SELECT v.patient_id\n" +
+			"                    FROM kenyaemr_etl.etl_hei_follow_up_visit v\n" +
+			"                    WHERE v.dna_pcr_result IS NOT NULL\n" +
+			"                       OR v.first_antibody_result IS NOT NULL\n" +
+			"                       OR v.final_antibody_result IS NOT NULL\n" +
+			"                    GROUP BY v.patient_id) has_test ON e.patient_id = has_test.patient_id\n" +
+			"         LEFT JOIN (SELECT e.patient_id\n" +
+			"                    from kenyaemr_etl.etl_hiv_enrollment e\n" +
+			"                             INNER JOIN kenyaemr_etl.etl_patient_demographics d on e.patient_id = d.patient_id\n" +
+			"                    WHERE visit_date <= CURRENT_DATE) hiv_prog\n" +
+			"                   ON e.patient_id = hiv_prog.patient_id\n" +
+			"WHERE TIMESTAMPDIFF(MONTH, d.dob, DATE_SUB(CURRENT_DATE, INTERVAL 7 DAY)) = 24\n" +
+			"  AND has_test.patient_id IS NULL\n" +
+			"  AND hiv_prog.patient_id IS NULL;";
+
+		try {
+			Context.addProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+			Long hei24MonthsWithoutDocumentedOutcomeTotal = (Long) Context.getAdministrationService().executeSQL(hei24MonthsWithoutDocumentedOutcomeQuery, true).get(0).get(0);
+			return hei24MonthsWithoutDocumentedOutcomeTotal;
+
+		}
+		finally {
+			Context.removeProxyPrivilege(PrivilegeConstants.SQL_LEVEL_ACCESS);
+		}
+	}
+	
+	
+}
