db23b8ea0bb312208ff23a5f3ced18e122e316fa
gabu90 <gabriel.mbatha@gmail.com>
2025-06-16 11:42:07 +0300

    concepts as global properties for easy configuration

--- api/src/main/java/org/openmrs/module/kenyaemr/advice/OutpatientToInpatientCheckinOnAdmissionRequest.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/advice/OutpatientToInpatientCheckinOnAdmissionRequest.java
@@ -17,6 +17,7 @@ import org.openmrs.User;
 import org.openmrs.Visit;
 import org.openmrs.api.VisitService;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.kenyaemr.EmrConstants;
 import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
 import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.springframework.aop.AfterReturningAdvice;
@@ -30,14 +31,6 @@ import java.util.*;
 public class OutpatientToInpatientCheckinOnAdmissionRequest implements AfterReturningAdvice {
 
     private Log log = LogFactory.getLog(this.getClass());
-
-    public static final String INPATIENT_ADMISSION_REQUEST_QUESTION_CONCEPT = "160433AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    public static final String INPATIENT_ADMISSION_ANSWER_CONCEPT = "1654AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-    private static final Set<Integer> CONDITIONS_CONCEPTS = new HashSet<>(Arrays.asList(114662,115728,149019,117789,117855,2005642,127417, 2008582, 136373,145438, 119692,118631,126513,116004, 128653, 128825, 115968, 113701, 115986, 2003295, 128788, 113744, 116017, 116041, 113689,
-            128988, 2003169, 129069, 113763, 128781,2003211, 116049, 116066, 128955, 113712, 2003278, 157622, 126901,1295,2010623,115115,142474, 2004524, 134721, 2004525, 2004526,2004527, 2004325, 2004326, 2004327, 2004329,2004330, 2004331, 2004332,2004335,121610, 2010615, 125971,
-            2010616,  2010617, 121960, 2010621, 2010622 ,143320, 2010623, 115001, 2010618, 121918, 2010619, 158974, 2010620, 2010624, 2010625, 2010626, 2010627, 119821, 2010628, 2010629,2010630,2010631, 2010632, 2010633, 2010634, 2010635, 2010636,145438,117321,
-            143893, 2004493, 2004494, 2004495, 2004496, 2004497, 2004498, 2004499, 2004500, 2004501,145131,140987, 113087,2010574, 2010580, 2010586,121375,2004715,2010437,2002858,2002837,2002460,2004420,2007861,126513,2005019));
-
     @Override
     public void afterReturning(Object returnValue, Method method, Object[] args, Object target) throws Throwable {
 
@@ -46,7 +39,7 @@ public class OutpatientToInpatientCheckinOnAdmissionRequest implements AfterRetu
             VisitService visitService = Context.getVisitService();
             if (enc != null && enc.getVisit() != null && enc.getVisit().getVisitType().getUuid().equals(CommonMetadata._VisitType.OUTPATIENT) && enc.getForm() != null && CommonMetadata._Form.CLINICAL_ENCOUNTER.equalsIgnoreCase(enc.getForm().getUuid())) {
                 for (Obs o : enc.getAllObs()) {
-                    if (o.getConcept().getUuid().equals(INPATIENT_ADMISSION_REQUEST_QUESTION_CONCEPT) && o.getValueCoded().getUuid().equals(INPATIENT_ADMISSION_ANSWER_CONCEPT)) {
+                    if (o.getConcept().getUuid().equals(EmrConstants.INPATIENT_ADMISSION_REQUEST_QUESTION_CONCEPT) && o.getValueCoded().getUuid().equals(EmrConstants.INPATIENT_ADMISSION_ANSWER_CONCEPT)) {
                         // end the OPD visit
                         Visit opdVisit = enc.getVisit();
                         opdVisit.setStopDatetime(new Date());
@@ -81,7 +74,7 @@ public class OutpatientToInpatientCheckinOnAdmissionRequest implements AfterRetu
                     for (List<Object> row : diagnosisResults) {
                         if (row != null && !row.isEmpty()) {
                             Integer diagnosisCoded = (Integer) row.get(0);
-                            if (CONDITIONS_CONCEPTS.contains(diagnosisCoded)) {
+                            if (EmrConstants.CONDITIONS_CONCEPTS.contains(diagnosisCoded)) {
                                 // Query to check if the condition already exists in the conditions table
                                 String checkConditionQuery = String.format(
                                         "SELECT condition_id FROM conditions " +
