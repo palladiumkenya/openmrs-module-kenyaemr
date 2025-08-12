88ae6b6651a13c4a0a97756333785c038d67b191
ronald-pro <ronaldodhiambo27@gmail.com>
2025-08-12 15:38:45 +0300

    updated cohort queries

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/library/MOH505/Moh505CohortLibrary.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/library/MOH505/Moh505CohortLibrary.java
@@ -130,7 +130,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition patientDiagnosis(int diagnosis) {
-        String sqlQuery = "select patient_id from encounter_diagnosis where diagnosis_coded = " + diagnosis + " and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from encounter_diagnosis where diagnosis_coded = " + diagnosis + " and dx_rank = '2' and date(date_created) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("patientsDiagnosisCases");
         cd.setQuery(sqlQuery);
@@ -143,7 +143,7 @@ public class Moh505CohortLibrary {
     public CohortDefinition patientDeceased(int diagnosis, int patientOutcome) {
         String sqlQuery = "select d.patient_id from openmrs.encounter_diagnosis d\n" +
                 "inner join kenyaemr_etl.etl_clinical_encounter c on d.patient_id = c.patient_id\n" +
-                "where d.diagnosis_coded = " + diagnosis + " and c.patient_outcome = " + patientOutcome + "\n" +
+                "where d.diagnosis_coded = " + diagnosis + " and d.dx_rank = '2' and c.patient_outcome = " + patientOutcome + "\n" +
                 "and date(c.visit_date) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("patientsDeceased");
@@ -157,8 +157,8 @@ public class Moh505CohortLibrary {
     public CohortDefinition malariaTest(int diagnosis, int mRDT) {
         String sqlQuery = "select ed.patient_id from openmrs.encounter_diagnosis ed\n" +
                 "inner join kenyaemr_etl.etl_laboratory_extract le on ed.patient_id = le.patient_id\n" +
-                "where ed.diagnosis_coded = " + diagnosis + " and le.lab_test = " + mRDT + "\n" +
-                "and date(ed.date_created) between date(:startDate) and date(:endDate);";
+                "where ed.diagnosis_coded = " + diagnosis + " and ed.dx_rank = '2' and le.lab_test = " + mRDT + "\n" +
+                "and date(le.date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("mRDT Test");
         cd.setQuery(sqlQuery);
@@ -171,8 +171,8 @@ public class Moh505CohortLibrary {
     public CohortDefinition malariaTestPositive(int diagnosis, int mRDT) {
         String sqlQuery = "select ed.patient_id from openmrs.encounter_diagnosis ed\n" +
                 "inner join kenyaemr_etl.etl_laboratory_extract le on ed.patient_id = le.patient_id\n" +
-                "where ed.diagnosis_coded = " + diagnosis + " and le.lab_test = " + mRDT + " and le.result_name = 'POSITIVE'\n" +
-                "and date(ed.date_created) between date(:startDate) and date(:endDate);";
+                "where ed.diagnosis_coded = " + diagnosis + " and ed.dx_rank = '2' and le.lab_test = " + mRDT + " and le.result_name = 'POSITIVE'\n" +
+                "and date(le.date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("mRDT Test Positive");
         cd.setQuery(sqlQuery);
@@ -183,7 +183,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition typhoidTest(int typhoidTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + typhoidTest + " and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + typhoidTest + " and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("typhoidTest");
         cd.setQuery(sqlQuery);
@@ -194,7 +194,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition typhoidTestPositive(int typhoidTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + typhoidTest + " and result_name = 'POSITIVE' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + typhoidTest + " and result_name = 'POSITIVE' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("typhoidPositive");
         cd.setQuery(sqlQuery);
@@ -205,7 +205,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition tbTest(int tbTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + tbTest + " and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + tbTest + " and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("tbTest");
         cd.setQuery(sqlQuery);
@@ -216,7 +216,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition tbPositive(int tbTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + tbTest + " and result_name = 'POSITIVE' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + tbTest + " and result_name = 'POSITIVE' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("tbPositive");
         cd.setQuery(sqlQuery);
@@ -227,7 +227,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition dysentryTest(int dysentryTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + dysentryTest + " and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + dysentryTest + " and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("ShigellaDysentryTest");
         cd.setQuery(sqlQuery);
@@ -238,7 +238,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition dysentryTestPositive(int dysentryTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + dysentryTest + " and result_name LIKE '%positive%' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + dysentryTest + " and result_name LIKE '%positive%' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("ShigellaDysentryTestPositive");
         cd.setQuery(sqlQuery);
@@ -249,7 +249,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition maternalDeath() {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_mchs_delivery where maternal_death_audited = '1065' and date(date_last_modified) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_mchs_delivery where maternal_death_audited = '1065' and date(visit_date) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("maternal");
         cd.setQuery(sqlQuery);
@@ -260,7 +260,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition bacterialTest(int bacteriaTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + bacteriaTest + " and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + bacteriaTest + " and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("BacterialMeningitis");
         cd.setQuery(sqlQuery);
@@ -271,7 +271,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition bacterialTestPositiveNM(int bacteriaTest1, int bacteriaTest2) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where (lab_test = " + bacteriaTest1 + " or lab_test = " + bacteriaTest2 + " ) and result_name LIKE '%+ve Nm%' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where (lab_test = " + bacteriaTest1 + " or lab_test = " + bacteriaTest2 + " ) and result_name LIKE '%+ve Nm%' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("BacterialMeningitis(+ve Nm)");
         cd.setQuery(sqlQuery);
@@ -282,7 +282,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition bacterialTestPositiveSp(int bacteriaTest1, int bacteriaTest2) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where (lab_test = " + bacteriaTest1 + " or lab_test = " + bacteriaTest2 + " ) and result_name LIKE '%+ve Sp%' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where (lab_test = " + bacteriaTest1 + " or lab_test = " + bacteriaTest2 + " ) and result_name LIKE '%+ve Sp%' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("BacterialMeningitis(+ve Sp)");
         cd.setQuery(sqlQuery);
@@ -293,7 +293,7 @@ public class Moh505CohortLibrary {
     }
 
     public CohortDefinition bacterialTestPositiveHInfluenza(int bacteriaTest) {
-        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + bacteriaTest + " and result_name LIKE '%+ve H influenza%' and date(date_created) between date(:startDate) and date(:endDate);";
+        String sqlQuery = "select patient_id from kenyaemr_etl.etl_laboratory_extract where lab_test = " + bacteriaTest + " and result_name LIKE '%+ve H influenza%' and date(date_test_requested) between date(:startDate) and date(:endDate);";
         SqlCohortDefinition cd = new SqlCohortDefinition();
         cd.setName("BacterialMeningitis(+ve H influenza)");
         cd.setQuery(sqlQuery);
